790a1ae00d30e070fa2166ccb5ff1f022a117c20
Oksana Furman <oksan9a.furman@gmail.com>
2023-05-09 17:14:19 +0200

    update files

--- js/main.js
+++ js/main.js
@@ -9,10 +9,12 @@ function printTasks(){
     box.innerHTML = "";
     for (let val of allTasks) {
         let color = "";
-        if (val.importance <=1) {
+        if (val.importance == 0) {
             color = "green";
-        } else if (val.importance <= 3) {
+        } else if (val.importance <= 2) {
             color = "yellow";
+        } else if (val.importance <= 4) {
+            color = "orange";
         }  else {
             color = "red";
         }
@@ -20,46 +22,95 @@ function printTasks(){
     
     //card with tasks, displayed in the page
     box.innerHTML += `
-        <div class="m-auto my-3">
-            <div class="card" style="width: 18rem;">
-                <img src="${val.picture}" class="card-img-top" alt="${val.title}">
-                <div class="card-body">
-                    <h5 class="card-title">${val.title}</h5>
-                    <a href="#" class="btn btnImportance" style="background-color: ${color};">${val.importance}</a>
-                    <p class="card-text">${val.deadline}</p>
+        <div class="item" style="background-image: url('${val.picture}'); background-position: center; background-repeat: no-repeat; background-size: cover;">
+            <div class="inner">
+                <h5 class="card-title">${val.title}</h5>
+                <i class="bi bi-exclamation-triangle"></i> <button class="btn btnImportance" style="background-color: ${color};">${val.importance}</button>
+                <div class="details">
+                    <p class="card-text"><i class="bi bi-calendar-day"></i> ${val.deadline}</p>
                     <p class="card-text">${val.description}</p>
-                    <a href="#" class="btn btn-success btnDone">Done</a>
-                    <a href="#" class="btn btn-danger btnDelete">Delete</a>
+                    <div class="btnDiv">
+                        <button class="btn btn-success btnDone m-1">Done</button>
+                        <button class="btn btn-danger btnDelete m-1">Delete</button>
+                    </div>
                 </div>
             </div>
         </div>`;
+
+    // box.innerHTML += `
+    //     <div class="item m-auto my-3">
+    //         <div class="card" style="width: 18rem;">
+    //             <img src="${val.picture}" class="card-img-top" alt="${val.title}">
+    //             <div class="card-body">
+    //                 <h5 class="card-title">${val.title}</h5>
+    //                 <i class="bi bi-exclamation-triangle"></i> <button class="btn btnImportance" style="background-color: ${color};">${val.importance}</button>
+    //                 <p class="card-text"><i class="bi bi-calendar-day"></i> ${val.deadline}</p>
+    //                 <p class="card-text">${val.description}</p>
+    //                 <button class="btn btn-success btnDone">Done</button>
+    //                 <button class="btn btn-danger btnDelete">Delete</button>
+    //             </div>
+    //         </div>
+    //     </div>`;
+    }
+
+    // button changes importance level(number and color)
+    let btnsImportance = document.getElementsByClassName('btnImportance');
+    for (let i = 0; i < btnsImportance.length; i++) {
+        btnsImportance[i].addEventListener("click", function() {
+            allTasks[i].importance++;
+            if (allTasks[i].importance > 5) {
+                allTasks[i].importance = 0;
+            }
+            document.getElementsByClassName('btnImportance')[i].innerHTML = allTasks[i].importance;
+            let color = "";
+            if (parseInt(document.getElementsByClassName("btnImportance")[i].innerHTML) == 0) {
+                color = "green";
+            } else if (parseInt(document.getElementsByClassName("btnImportance")[i].innerHTML) <= 2) {
+                color = "yellow";
+            } else if (parseInt(document.getElementsByClassName("btnImportance")[i].innerHTML) <= 4) {
+                color = "orange";
+            } else {
+                color = "red";
+            }
+            document.getElementsByClassName("btnImportance")[i].style.backgroundColor = color;
+        })  
+    }
+
+    let cardArray = document.getElementsByClassName('item');
+    let btnsDone = document.getElementsByClassName('btnDone');
+    let inner = document.getElementsByClassName('inner');
+    for (let i = 0; i < btnsDone.length; i++) {
+        btnsDone[i].addEventListener("click", function() {
+            cardArray[i].style.filter = "grayscale(100%)";
+            btnsDone[i].style.display = "none";
+            inner[i].style.transform = "rotateY(360deg)";
+            inner[i].style.transition = "transform 0.8s";
+        })
+    }
+    
+    let btnsDelete = document.getElementsByClassName('btnDelete');
+    for (let i = 0; i < btnsDelete.length; i++) {
+        btnsDelete[i].addEventListener("click", function() {
+            cardArray[i].style.display = "none";
+        })
+    }
+    
 }
-}
-
-let btnsImportance = document.getElementsByClassName('btnImportance');
-for (let i = 0; i < btnsImportance.length; i++) {
-    btnsImportance[i].addEventListener("click", function() {
-        allTasks[i].importance++;
-        if (allTasks[i].importance > 5) {
-            allTasks[i].importance = 0;
-        }
-        console.log(allTasks[i].importance);
-        document.getElementsByClassName('btnImportance')[i].innerHTML = allTasks[i].importance;
-        // let color = "";
-        // if (parseInt(document.getElementsByClassName("btnImportance")[i].innerHTML) <= 1) {
-        //     color = "green";
-        // } else if (parseInt(document.getElementsByClassName("btnImportance")[i].innerHTML) <= 3) {
-        //     color = "yellow";
-        // } else {
-        //     color = "red";
-        // }
-        // document.getElementsByClassName("btnImportance")[i].style.backgroundColor = color;
-    })  
-}
-
-let btnsDone = document.getElementsByClassName('btnDone');
-
-
-let btnsDelete = document.getElementsByClassName('btnDelete');
+    let btnSortDown = document.getElementById('btnSortDown');
+    
+    btnSortDown.addEventListener("click", function(){
+        allTasks.sort((a, b) => b.importance - a.importance);
+        btnSortDown.style.display = "none";
+        btnSortUp.style.display = "inline";
+        printTasks();
+    });
+    let btnSortUp = document.getElementById('btnSortUp');
+    btnSortUp.addEventListener("click", function(){
+        allTasks.sort((a, b) => a.importance - b.importance);
+        btnSortUp.style.display = "none";
+        btnSortDown.style.display = "inline";
+        printTasks();
+    });
+    
 
 printTasks();
--- js/tasks.js
+++ js/tasks.js
@@ -1,91 +1,91 @@
 tasks = `[{
     "title": "Shopping",
     "description": "Get groceries from Hofer.",
-    "importance": "1",
+    "importance": "0",
     "deadline": "10.05.2023",
-    "picture": ""
+    "picture": "../img/homework.jpg"
 }, {
     "title": "Homework",
     "description": "Finish second code review.",
-    "importance": "3",
+    "importance": "0",
     "deadline": "08.05.2023",
-    "picture": ""
+    "picture": "../img/homework.jpg"
 }, {
     "title": "Cooking",
     "description": "Cook something with mushrooms before they go bad.",
-    "importance": "5",
+    "importance": "0",
     "deadline": "12.05.2023",
-    "picture": ""
+    "picture": "../img/homework.jpg"
 }, {
     "title": "Baking",
     "description": "Bake earl gray cookies.",
     "importance": "0",
     "deadline": "09.05.2023",
-    "picture": ""
+    "picture": "../img/homework.jpg"
 }, {
     "title": "Laundry",
     "description": "Wash bed sheets at 20.00, 08.05.2023.",
     "importance": "0",
     "deadline": "08.05.2023",
-    "picture": ""
+    "picture": "../img/homework.jpg"
 }, {
     "title": "Ironing",
     "description": "Iron and put away clean laundry.",
     "importance": "0",
     "deadline": "10.05.2023",
-    "picture": ""
+    "picture": "../img/homework.jpg"
 }, {
     "title": "Cleaning",
     "description": "It's your turn to clean the room.",
     "importance": "0",
     "deadline": "11.05.2023",
-    "picture": ""
+    "picture": "../img/homework.jpg"
 }, {
     "title": "Sport",
     "description": "Go to the gym with Christoph.",
     "importance": "0",
     "deadline": "07.05.2023",
-    "picture": ""
+    "picture": "../img/homework.jpg"
 }, {
     "title": "Cinema",
     "description": "'GOOD' at 20.00 15.06,2023, Haydn Kino.",
     "importance": "0",
     "deadline": "15.06.2023",
-    "picture": ""
+    "picture": "../img/homework.jpg"
 }, {
     "title": "Sandman",
     "description": "Finish listening Sandman act 2.",
     "importance": "0",
     "deadline": "01.06.2023",
-    "picture": ""
-}, {
-    "title": "Woodwork",
-    "description": "Watch Christoph build something.",
-    "importance": "0",
-    "deadline": "07.05.2023",
-    "picture": ""
+    "picture": "../img/homework.jpg"
 }, {
     "title": "Gardening",
-    "description": "Water the plants and pland aubergines.",
+    "description": "Pland aubergines in the garden.",
     "importance": "0",
     "deadline": "07.05.2023",
-    "picture": ""
+    "picture": "../img/homework.jpg"
+}, {
+    "title": "Gardening",
+    "description": "Water the plants in the garden.",
+    "importance": "0",
+    "deadline": "07.05.2023",
+    "picture": "../img/homework.jpg"
 }, {
     "title": "Homework",
     "description": "Read 5 pages in german, do 5 exercises.",
     "importance": "0",
     "deadline": "07.05.2023",
-    "picture": ""
+    "picture": "../img/homework.jpg"
 }, {
     "title": "Laundry",
     "description": "Wash dresses and a wool shirt.",
     "importance": "0",
     "deadline": "12.05.2023",
-    "picture": ""
+    "picture": "../img/homework.jpg"
 }, {
     "title": "Sport",
     "description": "Go to the gym with Christoph.",
     "importance": "0",
     "deadline": "10.05.2023",
-    "picture": ""
+    "picture": "../img/homework.jpg"
 }]`;
